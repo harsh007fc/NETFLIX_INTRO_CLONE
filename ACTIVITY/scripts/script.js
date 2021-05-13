8cc2a4d434232d291ff26dbb46edb09e8288532f
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-05-13 23:20:53 +0530

    code added upto js added

---
+++ ACTIVITY/scripts/script.js
@@ -0,0 +1,35 @@
+let tabItems = document.querySelectorAll('.tab_item');
+const tabContentItems = document.querySelectorAll('.tab_content_item');
+
+// Select tab content item
+function selectItem(e) {
+	// Remove all show and border classes
+	removeBorder();
+	removeShow();
+	// Add border to current tab item
+	this.classList.add('tab_border');
+	// Grab content item from DOM
+    // console.log(this.id);
+	const tabContentItem = document.querySelector(`#${this.id}_content`);
+	// Add show class
+	tabContentItem.classList.add('show');
+}
+
+// Remove bottom borders from all tab items
+function removeBorder() {
+	tabItems.forEach(item => {
+		item.classList.remove('tab_border');
+	});
+}
+
+// Remove show class from all content items
+function removeShow() {
+	tabContentItems.forEach(item => {
+		item.classList.remove('show');
+	});
+}
+
+// Listen for tab item click
+tabItems.forEach(item => {
+	item.addEventListener('click', selectItem);
+});
